b56aa303dc85662c62d58948d687c58236f5a708
Rachel Brouzes <84203178+rachelbrouzes@users.noreply.github.com>
2022-07-20 15:29:44 -0700

    Added files for game!
    Added files for first version of game!

---
+++ js/main.js
@@ -0,0 +1,82 @@
+console.log('Your JS is linked up. Be the person you needed when you were little.')
+
+/*----- constants -----*/
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
+/*----- app's state (variables) -----*/
+
+let board;
+
+let turn = 'X';
+
+let win;
+
+/*----- cached element references -----*/
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+const messages = document.querySelector('h2');
+
+/*----- event listeners -----*/
+
+document.getElementById('reset-button').addEventListener('click', init);
+
+document.getElementById('board').addEventListener('click', handleTurn);
+
+/*----- functions -----*/
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+
+    render();
+
+    };
+
+    init();
+
+function render() {
+    board.forEach(function(mark, index){
+    squares[index].textContent = mark;
+    console.log(mark, index);
+    });
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    };
+
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    // if (turn === 'X') {
+    // turn = 'O' 
+    // } else {
+    // turn = 'X' 
+    // };
+    win = getWinner();
+    render();
+    };
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach((combo, index) => {
+    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+    winner = board[combo[0]];
+    }
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
+    };
